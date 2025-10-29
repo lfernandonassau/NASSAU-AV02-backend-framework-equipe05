5295bee767ca20e925bca208a4e0c307f03898c8
rafxys <rafaeltopster@gmail.com>
2025-10-29 16:09:58 -0300

    Componentes da tela de login alinhados, Tela de login estilizada e pequenas mudanças nos componentes do Button.

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,55 +1,70 @@
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 import { 
-        Wrapper,
         Column, 
         Row, 
         TitleLogin, 
         KanbanSubText, 
-        EsqueciSubText,  
-        TitleKanban, 
+        EsqueciSubText,   
         CriarContaText, 
         LoginContainer, 
         EmailEstilizado,
-        PasswordEstilizado
+        PasswordEstilizado,
+        PageWrapper,
+        LoginNewScreen,
+        WelcomeContainer,
+        TitleWelcome,
+        WelcomeSubText,
+        TitleKanban,
         } 
         from './styles'
 import { Link } from 'react-router-dom'
 const Login = () => {
     return (
         <>
-        <LoginContainer>
-            <Column>
-                <TitleKanban>
-                    Fase inicial da tela de login
-                </TitleKanban>
-            </Column>
-            <Column>
-                <Wrapper>
-                    <TitleLogin>
-                        Login
-                    </TitleLogin>
-                    <KanbanSubText>
-                        Faça o seu login agora!
-                    </KanbanSubText>
-                    <form>
-                        <Input placeholder="E-mail" leftIcon={<EmailEstilizado/>}/>
-                        <Input placeholder="Senha" type="password" leftIcon={<PasswordEstilizado/>}/>
+        <PageWrapper>
+            <LoginNewScreen>
+                <WelcomeContainer>
+                    <Column>
+                        <TitleWelcome>Seja bem-vindo!</TitleWelcome>
+                        <WelcomeSubText>
+                            Organize projetos ✍️, acompanhe tarefas 📋 e colabore com sua equipe 🧑‍💻 usando nosso intuitivo quadro Kanban. 
+                        </WelcomeSubText>
+                        <TitleKanban> - Equipe Kodan</TitleKanban>
+                        <WelcomeSubText>
+                            <br />
+                            Ainda não é cadastrado?
+                        </WelcomeSubText>
+                        <Button title="Clique aqui!" variant="secondary"/>
+                    </Column>
+                </WelcomeContainer>
+                <LoginContainer>
+                    <Column>
+                        <TitleLogin>
+                            Login
+                        </TitleLogin>
+                        <KanbanSubText>
+                            Faça o seu login agora!
+                        </KanbanSubText>
+                        <form>
+                            <Input placeholder="E-mail" leftIcon={<EmailEstilizado/>}/>
+                            <Input placeholder="Senha" type="password" leftIcon={<PasswordEstilizado/>}/>
+                            <Row>
+                                <EsqueciSubText>
+                                Esqueci minha senha
+                            </EsqueciSubText>
+                            </Row>
+                            <Button title="Entrar" ></Button>
+                        </form>
                         <Row>
-                            <EsqueciSubText>
-                            Esqueci minha senha
-                        </EsqueciSubText>
+                            <CriarContaText>
+                                Não tem uma conta ainda? <Link to="/">Criar conta</Link>
+                            </CriarContaText>
                         </Row>
-                        <Button title="Entrar" ></Button>
-                    </form>
-                    <Row>
-                        <CriarContaText>
-                            Não tem uma conta ainda? <Link to="/">Criar conta</Link>
-                        </CriarContaText>
-                    </Row>
-                </Wrapper>
-            </Column>
-        </LoginContainer>
+                    </Column>
+                </LoginContainer>
+            </LoginNewScreen>
+        </PageWrapper>
         </>
     )
 }
