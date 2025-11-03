678130793c1576f8932cfca11ecdff550d095ee1
Ryan_r <ryanrodrigues.dsantos@gmail.com>
2025-11-03 20:54:13 -0300

    estilização do footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
+import React from "react";
 import {
-    Container,
-    FooterContainer,
-    FooterText
-} from './styles'; 
+  FooterContainer,
+  FooterContent,
+  FooterSection,
+  FooterTitle,
+  FooterText,
+  FooterLink,
+  Copyright,
+} from "./styles";
 
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <FooterSection>
+          <FooterTitle>Sobre</FooterTitle>
 
+          <FooterText>
+            
+            Kodan é uma plataforma web desenvolvidacom que oferece um ambiente de gestão de projetos em formato Kanban , onde líderes e colaboradores podem criar projetos, organizar tarefas e acompanhar o progresso das equipes.
+            
+          </FooterText>
 
+        </FooterSection>
 
-const Footer = () => {
-    return (
-        <FooterContainer>
-            <Container>
-                <FooterText>Todos os direitos reservados, menos pro alan</FooterText>
-                <FooterText>Ass: Samuel Douglas Santos Sadousan</FooterText>
-            </Container>
-        </FooterContainer>
-    );
-}
+        <FooterSection>
+          <FooterTitle>Links úteis</FooterTitle>
+          <FooterLink href="#">Início</FooterLink>
+          <FooterLink href="#">Projetos</FooterLink>
+          <FooterLink href="#">Contato</FooterLink>
+        </FooterSection>
+
+        <FooterSection>
+          <FooterTitle>Contato</FooterTitle>
+          <FooterText>Email: samuel.douglas@SAudosan.com</FooterText>
+          <FooterText>
+            GitHub: <FooterLink href="#"> ... </FooterLink>
+          </FooterText>
+          <FooterText>
+            <FooterLink href="#"> ... </FooterLink>
+          </FooterText>
+          <FooterText>
+        <FooterLink href="#"> ... </FooterLink>
+          </FooterText>
+        </FooterSection>
+      </FooterContent>
+
+      <Copyright>
+        © {new Date().getFullYear()} copyright — Todos os direitos reservados
+      </Copyright>
+    </FooterContainer>
+  );
+};
 
 export default Footer;
