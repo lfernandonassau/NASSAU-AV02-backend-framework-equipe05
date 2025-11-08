5a29fff3f97f23015f2d3b727ac927a798ddec8b
rafxys <rafaeltopster@gmail.com>
2025-11-08 18:28:09 -0300

    Implementaçãol do scroll na page home, adaptação ao mobile finalizada, hierarquia dos components do header estruturadas

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,39 @@
+import { useState, useEffect } from 'react'
+
 import { Button } from '../../components/Button'
-import { Container, ImageContainer, TitleKanban, KanbanText, TitleColor, Wrapper } from './styles'
+import { Container, ImageContainer, TitleKanban, KanbanText, TitleColor, Wrapper, ParteDoisContainer, PartTwoText, ScrollDownIndicator, AbaixoIcon } from './styles'
 import { Header } from '../../components/Header'
 import banner from '../../assets/banner.svg'
 import Footer from '../../components/footer'
+
+
 const Home = () => {
+
+
+    const [showScroll, setShowScroll] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setShowScroll(false)
+            } else {
+                setShowScroll(true)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, []) //Array vazio = garantir que só rode uma vez
+
+    //Função para rolar a página
+    const handleScrollDown = () => {
+
+        window.scrollTo({
+            top: window.innerHeight - 47, // 47 altura do Header
+            behavior: 'smooth'
+        })
+    }
+
     return (
         <Wrapper>
             <Header />
@@ -19,7 +49,18 @@ const Home = () => {
                 <Button variant='secundary' title='Vamos começar!' onClick={() => null}/>
                 <ImageContainer src={banner} alt="Banner Kodan" />    
             </Container>
+            <ParteDoisContainer>
+                <PartTwoText>
+                    Do "A Fazer" ao "Feito", sem complicações.
+                </PartTwoText>
+                <ImageContainer src={banner} alt="Banner Kodan" />
+            </ParteDoisContainer>
             <Footer/>
+            {/* Usar $show (com cifrão) para o styled-components
+             entender que é uma "prop transitória" e não deve ir para o HTML */}
+            <ScrollDownIndicator $show={showScroll} onClick={handleScrollDown}>
+                <AbaixoIcon />
+            </ScrollDownIndicator>
         </Wrapper>
     )
 }
