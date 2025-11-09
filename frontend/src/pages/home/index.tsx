9b5d2dc26d6cebacef5d66f6b38496ffa074ebc7
AlandersonSantos <alandersonlima089@gmail.com>
2025-11-08 22:18:54 -0300

    Subindo a nova estrutura do banco

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <Wrapper>
             <Header />
             <Container>
-            
+
                 <TitleKanban>
                     Kodan
                     <TitleColor>
