337927831813beb9da7ef3fe8a8a0ee90a073bc1
AlandersonSantos <alandersonlima089@gmail.com>
2025-11-20 20:27:59 -0300

    Subindo as tabelas do banco de dados, e CRUD simples.

---
+++ backend/UserRepository.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function connect(){
+    await prisma.$connect();
+}
+
+connect();
+// Fazer teste com operador de comparaça OR
+//  ##CRUD do usuário:##
+
+//Consultador todos os usuários no banco de dados.
+export function GetUsers(){
+
+    return prisma.user.findMany();
+
+}
+
+//Consultar um usuário específico no banco de dados.
+export function GetUser(cpf : string){
+
+    return prisma.user.findUnique({
+        where: {cpf}
+
+    });
+
+}
+
+//Criar um novo usuário no banco de dados.
+export function CadastreUser(newUser : any){
+
+    return prisma.user.create({
+        data: newUser
+    })
+}
+
+//Atualizar um usuário específico no banco de dados.
+export function UpadateUser(cpf : string, newData : any){
+
+    return prisma.user.update({
+        where: { cpf },
+        data: newData
+    })
+}
+
+
+//Deletar um usuário específico no banco de dados.
+export function DeleteUser(cpf : string){
+
+    return prisma.user.delete({
+        where: {cpf}
+    })
+}
