b34bcb20e86ba1a59a8d913d8eca7c7196461b61
Ryan_r <ryanrodrigues.dsantos@gmail.com>
2025-11-22 01:01:00 -0300

    concerrtando o login com google e personalizando o PrivacyModel

--- frontend/src/components/footer/PrivacyModal/index.tsx
+++ frontend/src/components/footer/PrivacyModal/index.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import Button from "@mui/material/Button";
+import {
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Button,
+  Typography,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+import logo from "../../../assets/logo.svg";
+
+/* IMPORTA OS ESTILOS */
+import {
+  TitleRow,
+  LogoIcon,
+  TitleStyled,
+  CloseButton,
+  PaperStyled,
+} from "./styles";
 
 interface PrivacyModalProps {
   open: boolean;
@@ -12,31 +26,63 @@ interface PrivacyModalProps {
 
 const PrivacyModal: React.FC<PrivacyModalProps> = ({ open, onClose }) => {
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-      <DialogTitle>Políticas de Privacidade & Termos de Uso</DialogTitle>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="md"
+      fullWidth
+      PaperProps={{ sx: PaperStyled }}
+    >
+      {/* HEADER */}
+      <TitleRow>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <LogoIcon src={logo} alt="Kodan" />
+          <TitleStyled>
+            Políticas de Privacidade & Termos de Uso
+          </TitleStyled>
+        </div>
+
+        <CloseButton onClick={onClose}>
+          <CloseIcon fontSize="small" />
+        </CloseButton>
+      </TitleRow>
 
       <DialogContent dividers>
-        <p>
+        <Typography paragraph>
           Este site utiliza dados fornecidos pelos usuários exclusivamente para
           fins de funcionamento da plataforma Kodan. As informações não são
           compartilhadas com terceiros e permanecem protegidas conforme as leis
           vigentes.
-        </p>
+        </Typography>
 
-        <p>
+        <Typography paragraph>
           Ao utilizar nossa plataforma, você concorda com o uso de cookies,
           armazenamento de dados essenciais e procedimentos de segurança para
           garantir a integridade das informações.
-        </p>
+        </Typography>
 
-        <p>
+        <Typography paragraph>
           Para solicitações de remoção de dados, dúvidas ou demais informações,
           entre em contato com nossa equipe de suporte pelo e-mail informado.
-        </p>
+        </Typography>
       </DialogContent>
 
       <DialogActions>
-        <Button onClick={onClose}>Fechar</Button>
+        <Button
+          onClick={onClose}
+          variant="contained"
+          sx={{
+            backgroundColor: "#b9d9ff",
+            color: "#0b1b3a",
+            fontWeight: 700,
+            borderRadius: "10px",
+            "&:hover": {
+              backgroundColor: "#a8c5f0",
+            },
+          }}
+        >
+          Fechar
+        </Button>
       </DialogActions>
     </Dialog>
   );
