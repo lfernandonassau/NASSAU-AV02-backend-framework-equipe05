ea042bd9aeed890742ae2652c475a1fdd21bb495
Ryan_r <ryanrodrigues.dsantos@gmail.com>
2025-11-21 22:51:12 -0300

    Adicionando o Firebase ao projeto

--- frontend/src/pages/cadastro/index.tsx
+++ frontend/src/pages/cadastro/index.tsx
@@ -2,7 +2,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { api } from '../../services/api'
-
+import { signInWithPopup } from "firebase/auth";
 
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -31,10 +31,14 @@ import {
     RegisterNewScreen,
     TextoLivreSubText,
 } from './styles'
+
 import { IFormData } from './types'
 import { cpfMask } from '../../utils/cpfMask'
 import { nameMask } from '../../utils/nameMask'
 import { FcGoogle } from 'react-icons/fc'
+
+// IMPORTA SOMENTE DO ARQUIVO CORRETO (services/firebase.ts)
+import { auth, googleProvider } from '../../services/firebase'
 
 const schema = yup.object({
     cpf: yup.string().min(14, 'No minimo 11 caracteres').required('Campo obrigatório'),
@@ -42,34 +46,27 @@ const schema = yup.object({
     lastName: yup.string().min(3, 'Digite um sobrenome válido').required('Campo obrigatório'),
     email: yup.string().email('E-mail não é válido').required('Campo obrigatório'),
     password: yup.string().min(8, 'No minimo 8 caracteres').required('Campo obrigatório'),
-    // Comparar com o campo 'password'
-    confirmPassword: yup.string()
+    confirmPassword: yup
+        .string()
         .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
         .required('Confirmação obrigatória'),
 }).required()
 
 const Cadastro = () => {
-    
-    // Titulo visivel
-    const [estaVisivel, setEstaVisivel] = useState(false)
-    useEffect(() => {
-        // Define como visível após um pequeno atraso
-        // Isso dá tempo para o componente montar invisível
-        const timer = setTimeout(() => {
-        setEstaVisivel(true)
-        }, 200) // 200ms de atraso
 
-        // Limpa o timer se o componente for desmontado
+    const [estaVisivel, setEstaVisivel] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setEstaVisivel(true)
+        }, 200)
+
         return () => clearTimeout(timer)
     }, [])
 
-    //React-hook-form: Regras do formulário(Dentro do input)
-    const {control, handleSubmit , formState: { errors, isValid } } = useForm<IFormData>({
-        //verifica se realmente começou a validar ou não
+    const { control, handleSubmit, formState: { errors, isValid } } = useForm<IFormData>({
         resolver: yupResolver(schema),
-        //Assim que o usuário digitar ele já valida
         mode: 'onChange',
-        // É importante definir valores padrão vazios para evitar erro de "Uncontrolled component" por isso eu coloquei o defaultValues
         defaultValues: {
             name: '',
             lastName: '',
@@ -81,18 +78,14 @@ const Cadastro = () => {
     })
 
     const navigate = useNavigate()
+
     const onSubmit = async (formData: IFormData) => {
         try {
-            // Não precisa salvar isso no banco
             const { confirmPassword, ...userData } = formData
 
-            // TODO: Alterar para método POST para criar o usuário
-            // Exemplo: await api.post('/users', userData);
-            
             const { data } = await api.get(`users?email=${formData.email}`)
-            
-            if(data.length === 0){
-                // Aqui você faria o POST se o usuário não existir
+
+            if (data.length === 0) {
                 alert(`Usuário ${formData.name} cadastrado com sucesso! (Simulação)`)
                 navigate('/login')
             } else {
@@ -102,101 +95,134 @@ const Cadastro = () => {
             alert('Houve um erro, tente novamente')
         }
     }
-    
+
     const [showPassword, setShowPassword] = useState(false)
-    // Novo estado exclusivo para CONFIRMAR SENHA(Para não haver incongruências, como os dois olhos mágicos serem acionados)
     const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+    // FUNÇÃO DE LOGIN COM GOOGLE
+    const handleGoogleLogin = async () => {
+        console.log("Função chamada!") // DEBUG
+
+        try {
+            const result = await signInWithPopup(auth, googleProvider)
+            const user = result.user
+
+            console.log("Usuário logado:", user)
+            navigate('/perfil')
+
+        } catch (error) {
+            console.error("Erro ao logar com Google:", error)
+            alert("Erro ao entrar com Google.")
+        }
+    }
 
     return (
         <PageWrapper>
-            <RegisterNewScreen $visivel= {estaVisivel}>
+            <RegisterNewScreen $visivel={estaVisivel}>
                 <RegisterContainer>
                     <Column>
                         <TitleKanban>Crie sua conta</TitleKanban>
                         <KanbanSubText>Defina as informações necessárias 👌</KanbanSubText>
+
                         <FormContainer onSubmit={handleSubmit(onSubmit)}>
-                            <Input 
-                            name="cpf"
-                            placeholder="Digite seu CPF"
-                            control={control}
-                            errorMessage={errors?.cpf?.message}
-                            leftIcon={<CpfIconStyled/>} 
-                            mask={cpfMask}
+                            <Input
+                                name="cpf"
+                                placeholder="Digite seu CPF"
+                                control={control}
+                                errorMessage={errors?.cpf?.message}
+                                leftIcon={<CpfIconStyled />}
+                                mask={cpfMask}
                             />
-                            <Input 
-                            name="name"
-                            placeholder="Digite seu nome"
-                            control={control}
-                            errorMessage={errors?.name?.message}
-                            leftIcon={<NameIconStyled/>} 
-                            mask={nameMask} 
+
+                            <Input
+                                name="name"
+                                placeholder="Digite seu nome"
+                                control={control}
+                                errorMessage={errors?.name?.message}
+                                leftIcon={<NameIconStyled />}
+                                mask={nameMask}
                             />
-                            <Input 
-                            name="lastName"
-                            placeholder="Digite seu sobrenome"
-                            control={control}
-                            errorMessage={errors?.lastName?.message}
-                            leftIcon={<NameIconStyled/>}
-                            mask={nameMask} 
+
+                            <Input
+                                name="lastName"
+                                placeholder="Digite seu sobrenome"
+                                control={control}
+                                errorMessage={errors?.lastName?.message}
+                                leftIcon={<NameIconStyled />}
+                                mask={nameMask}
                             />
-                            <Input 
-                            name='email' 
-                            errorMessage={errors?.email?.message} 
-                            placeholder="Digite um e-mail" 
-                            control={control} 
-                            leftIcon={<LoginIconStyled/>}/>
-                            <Input 
-                            name='password' 
-                            errorMessage={errors?.password?.message} 
-                            placeholder="Digite uma senha" 
-                            control={control} 
-                            type={showPassword ? 'text' : 'password'} 
-                            leftIcon={<PasswordStyled/>} 
-                            rightIcon={showPassword ?
-                                    (<MagicEye onClick={() => setShowPassword(false)}/>) :
-                                    (<MagicEyeOff onClick={() => setShowPassword(true)}/>)
-                                }
+
+                            <Input
+                                name='email'
+                                errorMessage={errors?.email?.message}
+                                placeholder="Digite um e-mail"
+                                control={control}
+                                leftIcon={<LoginIconStyled />}
                             />
-                            <Input 
-                                name='confirmPassword' 
-                                placeholder="Digite novamente sua senha" 
-                                control={control} 
-                                errorMessage={errors?.confirmPassword?.message} 
-                                type={showConfirmPassword ? 'text' : 'password'} 
-                                leftIcon={<PasswordStyled/>}
+
+                            <Input
+                                name='password'
+                                errorMessage={errors?.password?.message}
+                                placeholder="Digite uma senha"
+                                control={control}
+                                type={showPassword ? 'text' : 'password'}
+                                leftIcon={<PasswordStyled />}
                                 rightIcon={
-                                    showConfirmPassword ? 
-                                    (<MagicEye onClick={() => setShowConfirmPassword(false)}/>) : 
-                                    (<MagicEyeOff onClick={() => setShowConfirmPassword(true)}/>)
+                                    showPassword
+                                        ? <MagicEye onClick={() => setShowPassword(false)} />
+                                        : <MagicEyeOff onClick={() => setShowPassword(true)} />
                                 }
                             />
-                            
+
+                            <Input
+                                name='confirmPassword'
+                                placeholder="Digite novamente sua senha"
+                                control={control}
+                                errorMessage={errors?.confirmPassword?.message}
+                                type={showConfirmPassword ? 'text' : 'password'}
+                                leftIcon={<PasswordStyled />}
+                                rightIcon={
+                                    showConfirmPassword
+                                        ? <MagicEye onClick={() => setShowConfirmPassword(false)} />
+                                        : <MagicEyeOff onClick={() => setShowConfirmPassword(true)} />
+                                }
+                            />
+
                             <Row>
-                                <PossuiContaSubText onClick= {() => { navigate('/login')}}>
+                                <PossuiContaSubText onClick={() => navigate('/login')}>
                                     Já possui uma conta? <a>Clique aqui</a>
                                 </PossuiContaSubText>
                             </Row>
-                            <Button 
-                            title='Entrar' 
-                            type='submit'
-                            variant='loginb'
-                            disabled={!isValid}>
-                            </Button>
+
+                            <Button
+                                title='Entrar'
+                                type='submit'
+                                variant='loginb'
+                                disabled={!isValid}
+                            />
+
                             <Row>
-                                <TextoLivreSubText>
-                                    ou
-                                </TextoLivreSubText>
+                                <TextoLivreSubText>ou</TextoLivreSubText>
                             </Row>
-                            <Button title='Entrar com Google' type='submit' variant="google" leftIcon={<FcGoogle/>}></Button>
+
+                            <Button
+                                title='Entrar com Google'
+                                type='button'
+                                variant="google"
+                                leftIcon={<FcGoogle />}
+                                onClick={handleGoogleLogin}
+                            />
+
                         </FormContainer>
                     </Column>
                 </RegisterContainer>
+
                 <WelcomeContainer>
                     <Column>
                         <WelcomeSubText>
                             <CloseButton>X</CloseButton>
                             <TitleWelcome>✅ Seja bem-vindo!</TitleWelcome>
-                            Diga adeus à desorganização. Cadastre-se no kodan e transforme a maneira como sua equipe trabalha 
+                            Diga adeus à desorganização. Cadastre-se no Kodan e transforme sua rotina!
                         </WelcomeSubText>
                     </Column>
                 </WelcomeContainer>
@@ -206,5 +232,3 @@ const Cadastro = () => {
 }
 
 export { Cadastro }
-
-
