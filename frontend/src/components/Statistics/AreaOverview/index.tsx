bada9dcbccb1f07a5f92842346becf3e6b98917c
Ryan_r <ryanrodrigues.dsantos@gmail.com>
2025-11-23 00:37:31 -0300

    criando a telaEstatisticas e seus componentes

---
+++ frontend/src/components/Statistics/AreaOverview/index.tsx
@@ -0,0 +1,64 @@
+import { ResponsiveLine } from "@nivo/line";
+import { CardContainer } from "../CardContainer";
+
+const data = [
+  {
+    id: "Tarefas",
+    data: [
+      { x: "S1", y: 5 },
+      { x: "S2", y: 9 },
+      { x: "S3", y: 14 },
+      { x: "S4", y: 11 },
+      { x: "S5", y: 16 },
+    ],
+  },
+];
+
+const AreaOverview = () => {
+  return (
+    <CardContainer style={{ height: 300 }}>
+      <h3>Evolução Geral</h3>
+
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 20, right: 20, bottom: 40, left: 50 }}
+        xScale={{ type: "point" }}
+        yScale={{ type: "linear" }}
+        enableArea={true}
+        areaOpacity={0.35}
+        colors={["#1454B8"]}
+        lineWidth={3}
+        pointSize={10}
+        pointBorderWidth={2}
+        pointBorderColor="#ffffff"
+        enableGridX={false}
+        enableGridY={true}
+        gridYValues={5}
+        theme={{
+          grid: {
+            line: {
+              stroke: "rgba(46, 46, 46, 0.2)",
+              strokeWidth: 1,
+            },
+          },
+          axis: {
+            ticks: {
+              text: {
+                fill: "#2E2E2E",
+              },
+            },
+            legend: {
+              text: {
+                fill: "#2E2E2E",
+              },
+            },
+          },
+        }}
+        useMesh={true}
+      />
+    </CardContainer>
+  );
+};
+
+export default AreaOverview;
+
