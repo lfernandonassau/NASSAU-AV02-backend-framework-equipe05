a2f5fca76fc91d64715e4a747778fc61fe3c3f0b
Sadousan <samueldouglas074@gmail.com>
2025-11-23 23:49:25 -0300

    Adicionando fluxo real de CRUD projetos

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import projectRoutes from './routes/project.routes.js'
 import userRoutes from './routes/user.routes.js'
 import columnRoutes from './routes/column.routes.js'
@@ -6,10 +7,17 @@ import jobRoutes from './routes/job.routes.js'
 import userpositionRoutes from './routes/userposition.routes.js'
 
 
+
 const app = express()
+
+// habilita CORS pro front do Vite
+app.use(cors({
+    origin: 'http://localhost:5173',
+}))
+
 app.use(express.json())
 
-app.use('/project', projectRoutes)
+app.use('/projects', projectRoutes)
 app.use('/user', userRoutes)
 app.use('/column', columnRoutes)
 app.use('/job', jobRoutes)
