7a9ccd2e97c9ad90fd9a4f89c386fd2330a7af30
Ryan_r <ryanrodrigues.dsantos@gmail.com>
2025-11-23 22:31:31 -0300

    corrigindo bug dos graficos

--- frontend/src/components/Statistics/Overview/index.tsx
+++ frontend/src/components/Statistics/Overview/index.tsx
@@ -21,18 +21,20 @@ const ProjectOverviewPie = () => {
           cornerRadius={5}
           activeOuterRadiusOffset={10}
           colors={["#47B0F7", "#1454B8", "#0B1B3A"]}
-          margin={{ top: 20, right: 90, bottom: 40, left: 40 }}
+          margin={{ top: 20, right: 80, bottom: 58, left: 80 }}
           enableArcLabels={false}
-          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsSkipAngle={8}
           arcLinkLabelsTextColor="#2E2E2E"
           arcLinkLabelsThickness={2}
           arcLinkLabelsColor={{ from: "color" }}
+          arcLinkLabelsDiagonalLength={14}
+          arcLinkLabelsStraightLength={14}
           legends={[]}
         />
-
       </ChartWrapper>
     </CardContainer>
   );
 };
 
 export default ProjectOverviewPie;
+
