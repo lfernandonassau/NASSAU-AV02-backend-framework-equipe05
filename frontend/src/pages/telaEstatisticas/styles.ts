25a548c9d03728e7695e9411e0de5b2315a58ae1
AlandersonSantos <alandersonlima089@gmail.com>
2025-11-24 21:46:58 -0300

    Subindo a estrutura do rotas update e delete, do usuário

--- frontend/src/pages/telaEstatisticas/styles.ts
+++ frontend/src/pages/telaEstatisticas/styles.ts
@@ -1,50 +1,45 @@
 import styled from "styled-components";
 
-/* ✅ Mesmo layout-base das outras telas */
-export const PageWrapper = styled.div`
-  display: flex;
+export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   background: #f5f7fa;
-`;
-
-export const Container = styled.div`
-  flex: 1;
   padding: 32px;
-  display: flex;
-  flex-direction: column;
-  gap: 32px;
-  margin-left: 385px; 
+  padding-left: 385px; 
+  box-sizing: border-box;
 `;
-
 
 export const Title = styled.h2`
   font-size: 22px;
   font-weight: 600;
-  color: #232323;
-  margin: 0;
-  text-align: left;
+  margin-bottom: 32px;
+  color: #0b1b3a;
 `;
 
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
   gap: 32px;
+  width: 100%;
 `;
 
 export const ChartsRow = styled.div`
   display: flex;
   gap: 24px;
+  width: 100%;
 
   > div {
     flex: 1;
-    height: 240px;
+    height: 260px;
   }
 `;
 
 export const BottomRow = styled.div`
   width: 100%;
-  height: 260px;
-  margin-top: 32px; 
+  height: 320px;
 `;
 
+export const CollaboratorSection = styled.div`
+  width: 100%;
+  margin-top: 32px;
+`;
